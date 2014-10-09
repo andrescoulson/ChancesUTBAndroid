1a9bc3b09fc6b95fa4b86409bc17bff97f3edc9e
andrescoulson <andresbobadillacoulson@gmail.com>
2014-10-09 14:18:18 -0500

    añadiendo vehiculos

---
+++ LoginTest/src/com/example/chances/RegisterVehicle.java
@@ -0,0 +1,23 @@
+package com.example.chances;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class RegisterVehicle extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		
+		View view = inflater.inflate(R.layout.activity_register_vehicle, container, false);
+		
+		return view;
+		
+	}
+	
+	
+
+}
