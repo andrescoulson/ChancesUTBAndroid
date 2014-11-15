705b8775a033c73f4fceabef33bdc9f5bd4fcb3d
AndresBobadilla <andresbobadillacoulson@gmail.com>
2014-11-15 14:02:15 -0500

    registro de vehiculos perfil de vehiculos y correciones

--- LoginTest/src/com/example/chances/RegisterVehicle.java
+++ LoginTest/src/com/example/chances/RegisterVehicle.java
@@ -5,20 +5,20 @@ import java.util.List;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.message.BasicNameValuePair;
-
-
-
-
-
-
-
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.FragmentManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.DataSetObserver;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,20 +30,122 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+import android.content.SharedPreferences;
 
 public class RegisterVehicle extends Fragment {
 
+	ListView List;
+	private static String url ="http://ing-sis.jairoesc.com/vehicle?auth-token=";
+	String token;
+	JSONObject jsonObject;
+	
+	String response;
 
+	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		
+		SharedPreferences Token  = getActivity().getSharedPreferences("token",Context.MODE_PRIVATE);
+		 token = Token.getString("auth_token", "NoToken");
+		
 		
 		View view = inflater.inflate(R.layout.activity_register_vehicle, container, false);
 		
+		VehicleGetTask task = new VehicleGetTask();
+		Log.e("url",url+token);
+		task.execute(url+token);
+		
+		
+		
+		ArrayList<ListaEntrada> datos = new ArrayList<ListaEntrada>();
+		datos.add(new ListaEntrada(R.drawable.ic_launcher, "Lorem ipsum",
+				"Lorem ipsum,Lorem ipsum,Lorem ipsum,Lorem ipsum"));
+		
+		List = (ListView)view.findViewById(R.id.listViewVehicle);
+		List.setAdapter(new com.example.chances.ListAdapter(getActivity(),
+				R.layout.entrada, datos) {
+			
+			@Override
+			public void onEntrada(Object entrada, View view) {
+				
+				TextView Text_Top_entrada = (TextView) view
+						.findViewById(R.id.textView_superior);
+				Text_Top_entrada.setText(((ListaEntrada) entrada)
+						.get_TextoEncima());
+
+				TextView Text_Below = (TextView) view
+						.findViewById(R.id.textView_inferior);
+				Text_Below.setText(((ListaEntrada) entrada).get_TextoDebajo());
+
+				ImageView Imagen = (ImageView) view
+						.findViewById(R.id.imageView_image);
+				Imagen.setImageResource(((ListaEntrada) entrada).get_IdImagen());
+				
+				
+			}
+		});
+		
+		List.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+		});
+		
 		return view;
+		
+	}
+	
+	private class VehicleGetTask extends AsyncTask<String, Void, String>{
+
+		@Override
+		protected String doInBackground(String... params) {
+			
+			String res=null;
+			try {
+				
+				response = CustomHttpClient.executeHttpGet(params[0]);
+				res = response.toString();
+				Log.e("devuelto por servidor", res.toString());
+				res = res.replaceAll("\\s+", "");
+				
+			} catch (Exception e) {
+				
+				Log.e("error",res);
+				
+			}
+			
+			return res;
+		}
+
+		@Override
+		protected void onPostExecute(String result) {
+			
+				try {
+				
+				
+				jsonObject = new JSONObject(result);
+				Log.e("JSONConvertido", jsonObject.toString());
+											
+
+			} catch (JSONException e) {
+				Log.e("ERROR", "JSONERROR");
+
+			}
+		}
+		
 		
 	}
 	
